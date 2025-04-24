9d35281421f12f852323c4385cca1a9bc62ef8df
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-04-24 17:30:18 +0300

    chore: remove radix components

--- app/src/components/app/ValueCard.tsx
+++ app/src/components/app/ValueCard.tsx
@@ -1,5 +1,4 @@
 import { twMerge } from "tailwind-merge";
-import { Card } from "../ui/card";
 
 export default function ValueCard({
   value,
@@ -17,13 +16,13 @@ export default function ValueCard({
   secondDescription?: string;
 }) {
   return (
-    <Card className="p-8 rounded-md flex flex-col gap-1 md:min-w-[230px] w-full">
+    <div className="p-8 rounded-md flex flex-col gap-1 md:min-w-[230px] w-full border shadow-md">
       <p className="font-light text-xl">{title}</p>
       <span className={twMerge("font-bold text-2xl", className)}>
         {icon} {value}
       </span>
       <span className="mt-3 ">{description}</span>
       <span className="mt-1 ">{secondDescription}</span>
-    </Card>
+    </div>
   );
 }

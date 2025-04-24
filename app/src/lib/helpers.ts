43d90036e0bbe66e746061da6c8b4b1f697b6426
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-04-24 18:52:46 +0300

    feat: add function to compute approximate amount of calories burned given a distance

--- app/src/lib/helpers.ts
+++ app/src/lib/helpers.ts
@@ -5,7 +5,7 @@ export const formatDate = (date: string) => {
 
   const formattedDate = format(
     parse(`${day} ${month} ${year}`, "d MMMM yyyy", new Date()),
-    "yyyy-MM-dd",
+    "yyyy-MM-dd"
   );
 
   return formattedDate;
@@ -14,3 +14,11 @@ export const formatDate = (date: string) => {
 export const earthCircumference = 40075;
 
 export const distanceToMoon = 384400;
+
+export const computeCaloriesBurned = (distance: number) => {
+  // Calories burned = Weight (kg) × Distance (km) × Calorie burn rate per kg/km
+  const weight = 85.5;
+  const burnRate = 0.9;
+
+  return weight * burnRate * distance;
+};

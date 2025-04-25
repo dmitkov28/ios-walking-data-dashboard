043b22ff44a70aa175ce947900c1169ca38053f8
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-04-25 11:46:35 +0300

    feat: add getDaysInCurrentYear helper function

--- app/src/lib/helpers.ts
+++ app/src/lib/helpers.ts
@@ -14,3 +14,11 @@ export const formatDate = (date: string) => {
 export const earthCircumference = 40075;
 
 export const distanceToMoon = 384400;
+
+
+
+export const getDaysInCurrentYear = () => {
+  const year = new Date().getFullYear();
+  const isLeap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+  return isLeap ? 366 : 365;
+}
